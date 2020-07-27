099bb0b708dbbebec013cb6ab781e6564890154e
rohitchandel1642 <rohitchandel1408@gmail.com>
2020-07-27 15:38:21 +0530

    Test1

---
+++ SLIDER/Assets/scrip.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrip : MonoBehaviour
+{
+    public float velocity = 1;
+    private Rigidbody sn;
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sn = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            sn.velocity = Vector2.up * velocity;
+            
+        }
+        sn.transform.position += new Vector3(sn.transform.position.x * speed * Time.deltaTime, 0, 0);
+    }
+}
